cf022970723ea074522535c430277f0e20ef6760
Mark Hayen <m.hayen@first8.nl>
2017-08-18 12:32:59 +0200

    replaced lombok @data

--- src/main/java/nl/moj/server/TeamDetailsService.java
+++ src/main/java/nl/moj/server/TeamDetailsService.java
@@ -15,24 +15,27 @@ import org.springframework.stereotype.Service;
 
 import nl.moj.server.model.Team;
 import nl.moj.server.persistence.TeamMapper;
+
 @Service
-public class TeamDetailsService implements UserDetailsService{
+public class TeamDetailsService implements UserDetailsService {
 	@Autowired
 	private TeamMapper teamMapper;
+
 	@Override
 	public UserDetails loadUserByUsername(String teamname) throws UsernameNotFoundException {
-		System.out.println("find team "+teamname+" out of "+teamMapper.findById(1l));
+		System.out.println("find team " + teamname + " out of " + teamMapper.findById(1l));
 		Team team = teamMapper.findByName(teamname);
 		System.out.println(team);
 		if (team == null)
-			throw new UsernameNotFoundException("no team found with name: "+teamname);
-		System.out.println("teamname: "+team.getName()+" password: "+team.getPassword());
+			throw new UsernameNotFoundException("no team found with name: " + teamname);
+		System.out.println("teamname: " + team.getName() + " password: " + team.getPassword());
 		return new User(teamname, team.getPassword(), true, true, true, true, getAuthorities(teamname));
 	}
+
 	public Collection<? extends GrantedAuthority> getAuthorities(String teamname) {
-	    List<GrantedAuthority> authList = new ArrayList<>();
-	    SimpleGrantedAuthority sGA = new SimpleGrantedAuthority(new String("ROLE_USER"));
-	    authList.add(sGA);
-	    return authList;
+		List<GrantedAuthority> authList = new ArrayList<>();
+		SimpleGrantedAuthority sGA = new SimpleGrantedAuthority(new String("ROLE_USER"));
+		authList.add(sGA);
+		return authList;
 	}
 }
