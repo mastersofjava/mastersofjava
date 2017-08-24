144a9adbf8464efde494b2540c03f5280e560011
HMolkenboer <h.molkenboer@first8.nl>
2017-08-24 13:40:20 +0200

    data.sql updated to accommodate Workloader

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO TEAM (name, password, role)
 VALUES ('team2', '$2a$10$/f3f9fZOExP7w2ewd0m7I.bvYFnCRMt7lYBEWiXkqVt/0COiWCpvu', 'ROLE_USER');
 
 INSERT INTO RESULT (team, assignment, score)
-VALUES ('team1', 'VirtualCPU', 0);
+VALUES ('team1', 'WorkloadBalancer', 0);
 
 INSERT INTO RESULT (team, assignment, score)
-VALUES ('team2', 'VirtualCPU', 0);
+VALUES ('team2', 'WorkloadBalancer', 0);
