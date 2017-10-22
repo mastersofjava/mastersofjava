aad9e92b239f88d464da9266ccb619fcdd84fe61
Mark Hayen <m.hayen@first8.nl>
2017-10-22 15:55:01 +0200

    creates directories on startup

--- src/main/java/nl/moj/server/Application.java
+++ src/main/java/nl/moj/server/Application.java
@@ -2,6 +2,9 @@ package nl.moj.server;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,23 +12,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	@Value("${moj.server.assignmentDirectory}")
 	public String assignmentDirectory;
 
 	@Value("${moj.server.teamDirectory}")
 	private String teamDirectory;
 
+	@Value("${moj.server.libDirectory}")
+	private String libDirectory;
+
 	@Value("${moj.server.basedir}")
 	private String basedir;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    
-    @PostConstruct
-    public void init() {
-    	
-    }
-    
+	@PostConstruct
+	public void init() {
+		if (!FileUtils.getFile(basedir, teamDirectory).exists()) {
+			FileUtils.getFile(basedir, teamDirectory).mkdir();
+		}
+		if (!FileUtils.getFile(basedir, assignmentDirectory).exists()) {
+			FileUtils.getFile(basedir, assignmentDirectory).mkdir();
+		}
+		if (!FileUtils.getFile(basedir, libDirectory).exists()) {
+			FileUtils.getFile(basedir, libDirectory).mkdir();
+		}
+
+	}
+
 }
