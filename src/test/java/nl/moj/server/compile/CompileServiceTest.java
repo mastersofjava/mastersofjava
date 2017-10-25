8d9aca0d18ae782b06de10a7100006b0d3e12c7d
ekaekebeke <e.kaekebeke@first8.nl>
2017-10-25 11:51:38 +0200

    Added Spring Boot test dependency to pom.xml

--- src/test/java/nl/moj/server/compile/CompileServiceTest.java
+++ src/test/java/nl/moj/server/compile/CompileServiceTest.java
@@ -1,11 +1,41 @@
 package nl.moj.server.compile;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import nl.moj.server.SubmitController;
+import nl.moj.server.TaskControlController;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CompileServiceTest {
 
+    @Autowired
+    private TaskControlController taskControlController;
 
+    @Autowired
+    private CompileService compileService;
+
+    @Test
+    public void doesNotCompileUncompilableCode() {
+
+        taskControlController.cloneAssignmentsRepo();
+        taskControlController.startTask(new TaskControlController.TaskMessage("VirtualCPU"));
+
+        SubmitController.SourceMessage sourceMessage = new SubmitController.SourceMessage();
+
+        sourceMessage.setTeam("test_team");
+
+        Map<String, String> source = new HashMap<>();
+        source.put("VirtualCPU", "");
+        sourceMessage.setSource(source);
+
+        compileService.compile(sourceMessage, false, false);
+    }
 
 }
