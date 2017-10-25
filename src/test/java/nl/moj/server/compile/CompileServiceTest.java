a14a6312a95258a8f1cf6a65039f129fca6b0761
ekaekebeke <e.kaekebeke@first8.nl>
2017-10-25 10:06:20 +0200

    Added Spring Boot test dependency to pom.xml

---
+++ src/test/java/nl/moj/server/compile/CompileServiceTest.java
@@ -0,0 +1,11 @@
+package nl.moj.server.compile;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class CompileServiceTest {
+
+
+
+}
