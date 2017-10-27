4d4c7a391f1c22b9d70495d54ee0910e4dab43a5
basp <b.passon@first8.nl>
2017-10-27 12:10:51 +0200

    removed default teams, updated password for control, see README.txt

--- src/main/resources/db/migration/V1.1__addcontroluser.sql
+++ src/main/resources/db/migration/V1.1__addcontroluser.sql
@@ -1,2 +1,3 @@
 INSERT INTO TEAM (name, password, role) 
-VALUES ('control', '$2a$10$keHr81N8GAFUoG2wTmQnCOWxlE1DYYbPDQSP6Re5zrgviPhoLn1E.', 'ROLE_CONTROL');
+VALUES ('control', '$2a$10$.kF3H9JfKnuozxIQ7XHTZOaOrvK1rZ8DIEHzZ.ZpulSfI/5RCJCf.', 'ROLE_CONTROL');
+
