1a53ef1ee34e175c4238c478f2b0a40974639c02
Mark Hayen <m.hayen@first8.nl>
2017-10-29 14:58:57 +0100

    added missing @Controller

--- src/main/java/nl/moj/server/FeedbackMessageController.java
+++ src/main/java/nl/moj/server/FeedbackMessageController.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
 import nl.moj.server.TaskControlController.TaskMessage;
 import nl.moj.server.compile.CompileResult;
 import nl.moj.server.test.TestResult;
 
+@Controller
 public class FeedbackMessageController {
 	
 	private static final Logger log = LoggerFactory.getLogger(FeedbackMessageController.class);
