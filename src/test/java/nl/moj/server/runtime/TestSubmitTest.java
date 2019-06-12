2c0fc6d24d5287986cd7781bfc692d750473eba0
Jan Groothuijse <jan@groothuij.se>
2019-06-12 17:27:08 +0200

    MOJ-87 Add test to check usage of a custom security-policy in an assignment

--- src/test/java/nl/moj/server/runtime/TestSubmitTest.java
+++ src/test/java/nl/moj/server/runtime/TestSubmitTest.java
@@ -9,6 +9,7 @@ import nl.moj.server.submit.SubmitResult;
 import nl.moj.server.submit.service.SubmitService;
 import nl.moj.server.submit.model.SourceMessage;
 import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,5 +74,42 @@ public class TestSubmitTest extends BaseRuntimeTest {
 	@After
 	public void cleanupCompetitionRuntime() {
 		competitionRuntime.stopCurrentAssignment();
+	}
+
+	@Test
+	public void shouldUseSpecifiedAssignmentTestTimeoutUseAssignmentSecurityPolicy() {
+
+		OrderedAssignment oa = getCompetition().getAssignments()
+				.stream()
+				.peek(System.out::println)
+				.filter( a -> a.getAssignment().getName().equals("assignment-2"))
+				.findFirst()
+				.orElseThrow();
+
+		competitionRuntime.startAssignment(oa.getAssignment().getName());
+
+		ActiveAssignment state = competitionRuntime.getActiveAssignment();
+		Duration timeout = state.getAssignmentDescriptor().getTestTimeout();
+		timeout = timeout.plus(mojServerProperties.getLimits().getCompileTimeout());
+
+		try {
+			Map<String,String> files = state.getAssignmentFiles().stream()
+					.filter( f -> f.getFileType() == AssignmentFileType.EDIT )
+					.collect(Collectors.toMap( f -> f.getUuid().toString(), AssignmentFile::getContentAsString));
+
+			SourceMessage src = new SourceMessage();
+			src.setSources(files);
+			src.setTests(Collections.singletonList(state.getTestFiles().get(0).getUuid().toString()));
+
+
+			SubmitResult submitResult = submitService.testAsync(getTeam(), src)
+					.get(timeout.plusSeconds(10).toSeconds(), TimeUnit.SECONDS);
+
+			Assertions.assertThat(submitResult.getTestResults().get(0).isSuccess()).isFalse();
+			Assertions.assertThat(submitResult.getTestResults().get(0).isTimeout()).isFalse();
+
+		} catch (Exception e) {
+			Assertions.fail("Caught unexpected exception.", e);
+		}
 	}
 }
