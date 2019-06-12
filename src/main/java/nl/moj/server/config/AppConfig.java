4224a06bedfe60e768e660a132862ee46a33f5ee
Jan Groothuijse <jan@groothuij.se>
2019-06-12 17:09:09 +0200

    MOJ-87 Add support to parse Optional in yaml files

--- src/main/java/nl/moj/server/config/AppConfig.java
+++ src/main/java/nl/moj/server/config/AppConfig.java
@@ -2,6 +2,7 @@ package nl.moj.server.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,7 @@ public class AppConfig {
 	public ObjectMapper yamlObjectMapper() {
 		ObjectMapper yamlObjectMapper = new ObjectMapper(new YAMLFactory());
 		yamlObjectMapper.registerModule(new JavaTimeModule());
+		yamlObjectMapper.registerModule(new Jdk8Module());
 
 		return yamlObjectMapper;
 	}
