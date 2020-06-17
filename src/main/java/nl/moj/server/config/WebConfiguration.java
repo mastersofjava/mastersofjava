0ade89aad650ca1945f4961ebca6265595572db2
Bob Kubista <b.kubista@first8.nl>
2020-06-17 13:21:33 +0200

    MOJ-165: being able to log in again

--- src/main/java/nl/moj/server/config/WebConfiguration.java
+++ src/main/java/nl/moj/server/config/WebConfiguration.java
@@ -33,6 +33,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -104,7 +105,18 @@ public class WebConfiguration {
 		protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
 			return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
 		}
+		private DaoAuthenticationProvider authProvider() {
+            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(teamDetailsService);
+            authProvider.setPasswordEncoder(passwordEncoder());
+            return authProvider;
+        }
 
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) {
+            auth.authenticationProvider(authProvider());
+        }
+        
 		@Bean
 		public PasswordEncoder passwordEncoder() {
 			return new BCryptPasswordEncoder();
