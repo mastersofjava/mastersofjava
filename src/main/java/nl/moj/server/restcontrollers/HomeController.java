446ca5beb5f01153ce45e7c5cb2ddf672ee1c528
Koen Aben <koaben@gmail.com>
2020-08-17 12:13:44 +0200

    [onderhoud] nieuwe pagina's voor onderscheid in  play/home/index

--- src/main/java/nl/moj/server/restcontrollers/HomeController.java
+++ src/main/java/nl/moj/server/restcontrollers/HomeController.java
@@ -1,5 +1,7 @@
 package nl.moj.server.restcontrollers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -7,11 +9,12 @@ import org.thymeleaf.context.WebContext;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@Controller
+@AllArgsConstructor
 public class HomeController  {
 
 
-    @GetMapping("/home")
+    @GetMapping("/index")
     public void process(HttpServletRequest request, HttpServletResponse response,
                             ServletContext servletContext, TemplateEngine templateEngine) {
 
