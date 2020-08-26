67bd99995cf41150741465715ce58eb7dd5a8224
Koen Aben <koaben@gmail.com>
2020-08-26 07:53:39 +0200

    Merge branch 'feature/keycloak-integration-leftovers' of https://bitbucket.org/first8/moj-game-server into feature/keycloak-integration-leftovers
    # Conflicts:
    #	src/test/java/nl/moj/server/IndexControllerTest.java

--- src/test/java/nl/moj/server/IndexControllerTest.java
+++ src/test/java/nl/moj/server/IndexControllerTest.java
@@ -1,13 +1,16 @@
 package nl.moj.server;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.when;
-
-import java.util.Collection;
-import java.util.List;
-
-import org.junit.Ignore;
+import nl.moj.server.assignment.repository.AssignmentRepository;
+import nl.moj.server.competition.model.CompetitionSession;
+import nl.moj.server.competition.repository.CompetitionSessionRepository;
+import nl.moj.server.competition.service.CompetitionService;
+import nl.moj.server.runtime.CompetitionRuntime;
+import nl.moj.server.runtime.repository.AssignmentStatusRepository;
+import nl.moj.server.teams.model.Team;
+import nl.moj.server.teams.repository.TeamRepository;
+import nl.moj.server.teams.service.TeamService;
+import nl.moj.server.user.model.User;
+import nl.moj.server.user.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,71 +20,83 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.ui.Model;
 
-import nl.moj.server.assignment.repository.AssignmentRepository;
-import nl.moj.server.competition.service.CompetitionService;
-import nl.moj.server.runtime.CompetitionRuntime;
-import nl.moj.server.runtime.repository.AssignmentStatusRepository;
-import nl.moj.server.teams.model.Team;
-import nl.moj.server.teams.repository.TeamRepository;
-import nl.moj.server.teams.service.TeamService;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@Ignore
 class IndexControllerTest {
 
 	@Mock
 	private CompetitionRuntime competition;
-    @Mock 
-    private TeamRepository teamRepository;
-    @Mock 
-    private TeamService teamService;
-    @Mock 
-    private AssignmentStatusRepository assignmentStatusRepository;
-    @Mock 
-    private AssignmentRepository assignmentRepository;
-    @Mock 
-    private CompetitionService competitionService;
-	
-    @InjectMocks
-    private GameController indexController;
-    
-	@Test
-	void testIndexNoUser() {
-		assertEquals("redirect:/sso/login", indexController.index(null, null, null));
+	@Mock
+	private TeamRepository teamRepository;
+	@Mock
+	private TeamService teamService;
+	@Mock
+	private AssignmentStatusRepository assignmentStatusRepository;
+	@Mock
+	private AssignmentRepository assignmentRepository;
+	@Mock
+	private CompetitionService competitionService;
+	@Mock
+	private CompetitionSessionRepository competitionSessionRepository;
+	@Mock
+	private UserService userService;
+
+	@InjectMocks
+	private GameController indexController;
+
+
+
+	private CompetitionSession createGameSession() {
+		CompetitionSession session = new CompetitionSession();
+		session.setUuid(UUID.randomUUID());
+		session.setId(1L);
+		return session;
 	}
 
 	@Test
 	void testIndexNewTeam() {
 		GrantedAuthority grantedAuthority = Mockito.mock(GrantedAuthority.class);
-		when(grantedAuthority.getAuthority()).thenReturn("ROLE_USER");
+		//  when(grantedAuthority.getAuthority()).thenReturn("ROLE_USER");
 		Collection<GrantedAuthority> grantedAuthorities = List.of(grantedAuthority);
-		
-		org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken user = Mockito.mock(org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken.class);
-		when(user.getName()).thenReturn("userName");
-		when(user.getAuthorities()).thenReturn(grantedAuthorities);
-		when(teamRepository.findByName(Mockito.anyString())).thenReturn(null);
-		
-		assertEquals("index", indexController.index(Mockito.mock(Model.class), user, null));
 
-		Mockito.verify(competitionService).addTeam(Mockito.any(Team.class));
+		org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken user = Mockito.mock(org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken.class);
+
+		// when(competition.getCompetitionSession()).thenReturn(createGameSession());
+		//when(user.getName()).thenReturn("userName");
+		// when(user.getAuthorities()).thenReturn(grantedAuthorities);
+		//when(teamRepository.findByName(Mockito.anyString())).thenReturn(null);
+		when(userService.createOrUpdate(user)).thenReturn(new User());
+		assertEquals("createteam", indexController.index(Mockito.mock(Model.class), user, null));
+
+		// Mockito.verify(competitionService).addTeam(Mockito.any(Team.class));
 	}
-	
+
 	@Test
 	void testIndexExistingTeam() {
 		GrantedAuthority grantedAuthority = Mockito.mock(GrantedAuthority.class);
-		when(grantedAuthority.getAuthority()).thenReturn("ROLE_USER");
+		//when(grantedAuthority.getAuthority()).thenReturn("ROLE_USER");
 		Collection<GrantedAuthority> grantedAuthorities = List.of(grantedAuthority);
-		
-		org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken user = Mockito.mock(org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken.class);
-		when(user.getName()).thenReturn("userName");
-		when(user.getAuthorities()).thenReturn(grantedAuthorities);
-		
+
+		org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken userToken = Mockito.mock(org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken.class);
+		User user = new User();
+		user.setTeam(Team.builder().name("test").build());
+		when(userService.createOrUpdate(userToken)).thenReturn(user);
+		when(competition.getCompetitionSession()).thenReturn(createGameSession());
+		// when(user.getName()).thenReturn("userName");
+
 		Team team = Team.builder()
 				.name("userName")
 				.build();
-		when(teamRepository.findByName(Mockito.anyString())).thenReturn(team );
-		
-		assertEquals("index", indexController.index(Mockito.mock(Model.class), user, null));
+//        when(teamRepository.findByName(Mockito.anyString())).thenReturn(team);
+
+		assertEquals("play", indexController.index(Mockito.mock(Model.class), userToken, null));
 
 		Mockito.verify(competitionService, never()).addTeam(Mockito.any(Team.class));
 	}
