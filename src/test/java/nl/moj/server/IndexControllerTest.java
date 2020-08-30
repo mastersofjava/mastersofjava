e96bd6c894d3ff32af901aafc348b027a6cc1fc5
Koen Aben <koaben@gmail.com>
2020-08-30 13:58:04 +0200

    [onderhoud] merge werkzaamheden (merge-conflict voorkomend).

--- src/test/java/nl/moj/server/IndexControllerTest.java
+++ src/test/java/nl/moj/server/IndexControllerTest.java
@@ -1,11 +1,1 @@
-package nl.moj.server;
 
-
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-class IndexControllerTest {
-
-}
