29ab61e8f217500e88a3234107f82b65f109f371
Bas Passon <b.passon@bliep.net>
2020-09-10 12:51:09 +0200

    MOJ-210: removed obsolete script

--- createUsers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..50}
-do
-    curl -X POST -F "name=team$i" -F "password=team$i" -F "cpassword=team$i" -F "country=Nederland" -F "company=First8" http://localhost:8080/register
-done
