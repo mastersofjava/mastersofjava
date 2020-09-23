8f880a9694e789827096ebdb3beb885677b24f27
Bas Passon <b.passon@bliep.net>
2020-09-23 16:39:49 +0200

    Removed commented code

--- src/main/resources/static/js/rankings.js
+++ src/main/resources/static/js/rankings.js
@@ -16,9 +16,6 @@ function connect() {
             + window.location.hostname
             + ':' + window.location.port
             + "/rankings/websocket",
-     //   debug: function (str) {
-   //         console.log(str);
-     //   },
         reconnectDelay: 5000,
         heartbeatIncoming: 4000,
         heartbeatOutgoing: 4000
